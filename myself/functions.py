50e9a3b6244d1971986d2617a4bd710eabc796d3
kimjunseok <jnunseok@gmail.com>
2018-12-27 17:17:53 +0900

    20181227selfwork

---
+++ myself/functions.py
@@ -0,0 +1,6 @@
+# split
+a = "Life is too short"
+b = a.split()
+c = "881120-1068234"
+d = c.split('c[5]') 
+print(d)
---
+++ myself/myself.py
@@ -0,0 +1,48 @@
+#1
+# print("Hello World")
+
+#2
+# A = int(input('A: '))
+# B = int(input('B: '))
+# if A <= 0 | A >= 10:
+#     print("A의 범위는 0과 10 사이 숫자입니다.")
+# elif B <= 0 | B >= 10:
+#     print("B의 범위는 0과 10 사이 숫자입니다.")
+# else:
+#    print(A+B)
+
+# def two_times(x): return x*2
+# A = list(map(two_times, [1, 2, 3, 4]))
+# print(A)
+# a = "Life is too short"
+# b = a.split()
+# print(b)
+
+a,b = list(map(int, input().split()))
+print(a+b)
+
+#3
+# A = int(input('A: '))
+# B = int(input('B: '))
+# if A <= 0 | A >= 10:
+#     print("A의 범위는 0과 10 사이 숫자입니다.")
+# elif B <= 0 | B >= 10:
+#     print("B의 범위는 0과 10 사이 숫자입니다.")
+# else:
+#    print(A-B)
+
+
+# A = int(input('A: '))
+# B = int(input('B: '))
+# if A <= 0 | A >= 10:
+#     print("A의 범위는 0과 10 사이 숫자입니다.")
+# elif B <= 0 | B >= 10:
+#     print("B의 범위는 0과 10 사이 숫자입니다.")
+# else:
+#    print(A/B)
+
+#4
+# print("맞은 수: 123 \n아이디: junseok")
+
+#5
+# print('|\\_/|\n''|q p|   /}\n''( 0 )"""\\\n''|"^"`    |\n''||_/=\\\\__|')
