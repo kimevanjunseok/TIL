25b9f6c3c5fb41d4295e3b502795fa3883d73db4
kimjunseok <jnunseok@gmail.com>
2019-01-03 17:51:06 +0900

    D02 | 190103 PM | Python Function

--- startcamp/myself/myself.py
+++ startcamp/myself/myself.py
@@ -48,17 +48,47 @@
 # print('|\\_/|\n''|q p|   /}\n''( 0 )"""\\\n''|"^"`    |\n''||_/=\\\\__|')
 
 #한수
-N = int(input())
-cnt = 99
-if N < 100:
-        print(N)
-else:
-        for i in range(100, N+1):
-                n_1 = i // 100
-                n_2 = (i % 100) // 10
-                n_3 = i % 10
-                if (n_1 - n_2) == (n_2 - n_3):
-                        cnt += 1
-        print(cnt)
+# N = int(input())
+# cnt = 99
+# if N < 100:
+#         print(N)
+# else:
+#         for i in range(100, N+1):
+#                 n_1 = i // 100
+#                 n_2 = (i % 100) // 10
+#                 n_3 = i % 10
+#                 if (n_1 - n_2) == (n_2 - n_3):
+#                         cnt += 1
+#         print(cnt)
 
 #별찍기 -11
+N = int(input())
+List = []
+List_2 = []
+a = '  *  '
+b = ' * * '
+c = '*****'
+List_2.append(a)
+List_2.append(b)
+List_2.append(c)
+List.append(a)
+List.append(b)
+List.append(c)
+cnt = N // 3
+num = 0
+for i in range(N):
+    if i > 2:
+        x = List_2[num] + ' ' + List_2[num]
+        List.append(x)
+        num += 1
+        if num == 3:
+            num = 0
+            for k in range(len(List)):
+                List_2.append(List[k].center(N*2-1))
+            for k in range(3):
+                del List_2[k]
+                print(k)
+    print(List[i].center(N*2-1))
+
+for i in range(6):
+    print(List_2[i])
