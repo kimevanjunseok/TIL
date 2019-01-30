c295a276984e4ca8dae1a06dbdfc62f5f95dab0b
kimevanjunseok <jnunseok@gmail.com>
2019-01-30 17:45:53 +0900

    FLASK

--- Flask/app.py
+++ Flask/app.py
@@ -1,20 +1,64 @@
 from flask import Flask, render_template
+import os
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '안녕하세요!!!'
+    return 'Hello World!!!'
+    
+# 5월 20일부터 d-day 카운트출력
+@app.route('/dday')
+def dday():
+    a = datetime.datetime(2019, 5, 20) - datetime.datetime.now()
+    return f'{a.days}일 남았습니다.'
+    
+# variable routing
+@app.route('/hi/<string:name>')
+def greeting(name):
+    return render_template('greeting.html', my_name=name)
 
-@app.route('/html')
-def html():
-    return render_template('chicken.html')
+@app.route('/cube/<int:number>')
+def num(number):
+    return f'{number}의 3제곱은 {number**3}입니다!!!!!!!'
+    
+# 반복문
+@app.route('/movie')
+def movie():
+    movies = ['극한직업', '정글북', '캡틴마블', '보헤미안랩소디', '완벽한타인']
+    return render_template('movie.html', movies=movies)
 
-@app.route('/html_name/<string:name>')
-def html_name(name):
-    return render_template('chicken.html', your_name = name)
 
-@app.route('/dictionary/<string:word>')
-def dictionary(word):
-    word_dict = {"apple": "사과", "banana": "바나나"}
-    return render_template('dictionary.html', word = word, word_dict = word_dict) 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+if __name__=='__main__':
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
