3ea18b1a3fe2457c9401768f89719965f871daf9
kimjunseok <jnunseok@gmail.com>
2019-02-12 22:59:58 +0900

    D08 | 190212 | algorithm

--- algorithm/D08/작업순서.py
+++ algorithm/D08/작업순서.py
@@ -1,46 +1,51 @@
 import sys
 sys.stdin = open("작업순서_input.txt")
 
-T = 6
+T = 10
 
 for n in range(1, T+1):
     V, E = map(int, input().split())
     route = list(map(int, input().split()))
     L = []
+    
     for i in range(0, len(route), 2):
         L += [[route[i], route[i+1]]]
+
     work = list(range(1, V+1))
+
     for i in L:
         if i[1] in work:
             work.remove(i[1])
-    cnt = 1
-    result = []
-    road = []
-    copy_L = L
-    while len(road) != V:
 
-        copy = []
-        if cnt == 1:
-            for i in range(len(L)):
+    result = work
 
-                if L[i][0] in work:
-                    road += L[i]
-                    copy_L.remove(L[i])
-                    copy += [L[i][1]]   
-        else:
-            for i in range(len(L)):
-                if L[i][0] in work:
-                    road += L[i][1]
-                    copy_L.remove(L[i])
-                    copy += [L[i][1]]
-        work = copy
-        L = copy_L
-        cnt += 1
+    while L:
+        long_L = len(L)
+        save = []
 
+        for i in range(long_L):
+            if L[i][0] in work:
+                save += [L[i]]
 
+        for i in save:
+            L.remove(i)
 
-        
+        work = list(range(1, V+1)) 
 
-        
+        for i in L:
+            if i[1] in work:
+                work.remove(i[1])
+
+        for i in work:
+            if i not in result:
+                result += [i]
+
+    print(len(result), V)
+
+    for i in range(len(result)):
+        result[i] = f'{result[i]}'
+
+    result = " ".join(result)
     
-    
+    print(f"#{n} {result}")
+
