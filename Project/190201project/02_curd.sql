068fd308d7dd298fb5860162f95cbc9fdb7a346a
kimevanjunseok <jnunseok@gmail.com>
2019-09-17 15:08:33 +0900

    deleted

--- Project/190201project/02_curd.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO movies
-VALUES(20182530, '극한직업', '15세이상관람가', '이병헌', 20190113, 3138467, 111, '한국', '코미디');
-SELECT * FROM movies WHERE 영화코드=20040521;
-DELETE FROM movies WHERE 영화코드=20040521;
-SELECT * FROM movies WHERE 영화코드=20185124;
-UPDATE movies
-SET 감독="없음"
-WHERE 영화코드=20185124;
-SELECT * FROM movies WHERE 영화코드=20185124;
--- Project/190201project/README.md
+++
@@ -1,8 +0,0 @@
-03_db/
-​	README.md
-​	01_create_table.sql
-​	02_crud.sql
-​	03_select.sql
-​	04_expression.sql
-​	05_order.sql
-​	pjt.sqlite3
